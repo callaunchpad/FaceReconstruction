6fceaae76ea0e968b39e3ce68094468bb080be51
wuisawesome <itswu.alex@gmail.com>
2018-02-09 19:43:51 -0800

    Test.py should print out lots of labels now

--- test.py
+++ test.py
@@ -4,7 +4,7 @@ files = fm.enumerate_data_paths()
 
 # print(len(list(files)))
 
-for i, file in zip(range(1), files):
+for i, file in zip(range(1000), files):
     print(file)
     pic, vertices = fm.get_datum(file)
     print(vertices)
