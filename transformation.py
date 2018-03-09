96d0fedabd372022e68a0e111c4ec50b75a933b7
Sahil Sancheti <ssancheti@berkeley.edu>
2018-03-08 17:12:46 -0800

    Added coordinate transformation code

---
+++ transformation.py
@@ -0,0 +1,30 @@
+import numpy as np
+
+def transform(x,y,z,rotation_matrix, translation_x=0, translation_y=0, translation_z=0):
+    translation = np.array([[translation_x], [translation_y], [translation_z]])
+    matrix = np.hstack((rotation_matrix,translation))
+    matrix = np.vstack((matrix, [0,0,0,1]))
+    result = matrix.dot(np.array([x,y,z,1]))
+    return result[:3]
+
+
+def rotate_x_axis(theta):
+    matrix = np.array([[1,0,0],
+                       [0,np.cos(theta),-np.sin(theta)],
+                       [0,np.sin(theta),np.cos(theta)]])
+
+    return matrix
+
+def rotate_y_axis(theta):
+    matrix = np.array([[np.cos(theta),0,np.sin(theta)],
+                       [0, 1, 0],
+                       [-np.sin(theta),0,np.cos(theta)]])
+    return matrix
+
+def rotate_z_axis(theta):
+    matrix = np.array([[np.cos(theta),-np.sin(theta),0],
+                       [np.sin(theta),np.cos(theta,0)],
+                       [0,0,1]])
+    return matrix
+
+print(transform(1,0,0,rotate_y_axis(0),1,0,0))
