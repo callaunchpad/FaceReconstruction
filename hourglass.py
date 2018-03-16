a73a5933f7693aee37a6b9f049e3eca048cdcbb1
Jihan Yin <jihan_yin@berkeley.edu>
2018-03-15 19:46:12 -0700

    umm idk

--- hourglass.py
+++ hourglass.py
@@ -15,18 +15,31 @@ Output
 '''
 def get_hourglass(features, layer_details, pool_details, residual_module):
     #our input is 200x200, a regular 2D image
-    input_layer = tf.reshape(features["x"], [-1, 200, 200, 1])
+    print(features["x"].shape)
+    input_layer = tf.reshape(features["x"], [-1, 200, 200, 3, 1])
+    new_conv_layer = tf.layers.conv3d(
+                                      inputs=input_layer,
+                                      filters=1,
+                                      kernel_size=[4, 4, 1],
+                                      padding="valid",
+                                      activation=tf.nn.relu)
+    pool_layer = tf.layers.max_pooling3d(inputs=new_conv_layer, pool_size=[10, 10, 1], strides=1)
+    print(input_layer.shape)
+    print(new_conv_layer.shape)
+    print(pool_layer.shape)
+    quit()
     #construct the first half of our downsampling convolutional layers, going off of layer_details and pool_details
     layers = [input_layer]
-    upsample_sizes = [(200, 200)]
+    upsample_sizes = [(200, 200, 3, 1)]
     for (kernel_dim1, kernel_dim2, nfilters, padding, activation), (pool_dim1, pool_dim2, stride) in zip(layer_details, pool_details):
-        new_conv_layer = tf.layers.conv2d(
+        new_conv_layer = tf.layers.conv3d(
             inputs=layers[-1],
             filters=nfilters,
             kernel_size=[kernel_dim1, kernel_dim2],
             padding=padding,
             activation=activation)
             #new_pool = tf.layers.max_pooling2d(inputs=new_conv_layer, pool_size=[pool_dim1, pool_dim2], strides=stride)
+        print(new_conv_layer.shape)
 
         #Each layer's size is dependent on their poolsize, and so we save these for upsampling later
         #upsample_sizes.append(layers.shape)
@@ -44,8 +57,6 @@ def get_hourglass(features, layer_details, pool_details, residual_module):
             new_upsample_layer = tf.reshape(new_upsample_layer, [-1, 200, 200, 1])
         #add residual layer
         residual_layer = residual_module(layers[-(2*(i+1))])
-        print(new_upsample_layer.shape)
-        print(residual_layer.shape)
         new_upsample_layer = tf.add(new_upsample_layer, residual_layer)
         layers.append(new_upsample_layer)
 
@@ -67,7 +78,7 @@ def get_kernel_size(layer_in, layer_out, padding="none"):
 
 def get_layer_size(layer_in, kernel, padding="none"):
     if padding == "none":
-        return (layer_in[0]-kernel[0]+1,layer_in[1]-kernel[1]+1)
+        return (layer_in[0]-kernel[0]+1,layer_in[1]-kernel[1]+1,layer_in[2]-kernel[2]+1)
 
 def hourglass_model_fn(features, labels, mode):
     layers = [(200-40*i, 200-40*i) for i in range(5)]
@@ -86,7 +97,7 @@ def hourglass_model_fn(features, labels, mode):
         return hourglass_model
     
     labels = tf.reshape(labels, [-1, 200, 200, 1])
-    loss = tf.losses.mean_squared_error(labels=labels,predictions=hourglass_model)
+    loss = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(logits=hourglass_model, labels=labels), name= 'cross_entropy_loss')
     if mode == tf.estimator.ModeKeys.TRAIN:
         optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.001)
         train_op = optimizer.minimize(loss=loss, global_step=tf.train.get_global_step())
@@ -101,19 +112,16 @@ def train_model(path, train_data, train_labels):
     facelift = tf.estimator.Estimator(model_fn=hourglass_model_fn, model_dir=path)
     train_input_fn = tf.estimator.inputs.numpy_input_fn(x={"x": train_data},
         y=train_labels,
-        batch_size=2,
+        batch_size=10,
         num_epochs=None,
         shuffle=True)
     facelift.train(input_fn=train_input_fn, steps=20000)
     return facelift
 
 
-train_data = np.array([np.random.rand(200, 200) for i in range(2)]).astype('float32')
-train_labels = np.array([np.random.rand(200, 200) for i in range(2)]).astype('float32')
+train_data = np.array([np.random.rand(200, 200, 3) for i in range(10)]).astype('float32')
+train_labels = np.array([np.random.rand(200, 200, 200) for i in range(10)]).astype('float32')
 model_path = "hourglass_util/"
-
-print(train_labels.shape)
-
 train_model(model_path, train_data, train_labels)
 
 
