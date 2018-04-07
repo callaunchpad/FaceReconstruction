7de5c03c31f5e522d0155cfac9feb9d8c2ffd665
wuisawesome <itswu.alex@gmail.com>
2018-04-06 18:17:47 -0700

    Uses conv2d instead of 3d

--- hourglass.py
+++ hourglass.py
@@ -16,55 +16,73 @@ Output
 def get_hourglass(features, layer_details, pool_details, residual_module):
     #our input is 200x200, a regular 2D image
     print(features["x"].shape)
-    input_layer = tf.reshape(features["x"], [-1, 200, 200, 3, 1])
+    input_layer = tf.reshape(features["x"], [-1, 200, 200, 3])
     #construct the first half of our downsampling convolutional layers, going off of layer_details and pool_details
-    layers = [input_layer]
-    upsample_sizes = [input_layer.get_shape().as_list()]
+    conv_layers = [input_layer]
+    # conv_input_shapes = [input_layer.get_shape()]
+    last_layer = input_layer
+    print("Generating hourglass conv layers")
     for (kernel_dim1, kernel_dim2, kernel_dim3, nfilters, padding, activation), (pool_dim1, pool_dim2, pool_dim3, stride) in zip(layer_details, pool_details):
-        new_conv_layer = tf.layers.conv3d(
-            inputs=layers[-1],
+        new_conv_layer = tf.layers.conv2d(
+            inputs=last_layer,
             filters=nfilters,
-            kernel_size=[kernel_dim1, kernel_dim2, kernel_dim3],
+            kernel_size=[kernel_dim1, kernel_dim2],
             padding=padding,
             activation=activation)
-        print("Original new conv layer shape:", new_conv_layer.shape)
-        new_conv_layer = tf.reshape(new_conv_layer, new_conv_layer.get_shape().as_list()[0:3]+[3,-1])
-        print("Resized new conv layer shape:", new_conv_layer.shape)
-        new_pool = tf.layers.max_pooling3d(inputs=new_conv_layer, pool_size=[pool_dim1, pool_dim2, pool_dim3], strides=stride)
-        print("New pool shape:", new_pool.shape)
+        # print("Original new conv layer shape:", new_conv_layer.shape)
+        # new_conv_layer = tf.reshape(new_conv_layer, new_conv_layer.get_shape().as_list()[0:3]+[3,-1])
+        # print(".")
+        # print("Resized new conv layer shape:", new_conv_layer.shape)
+        new_pool = tf.layers.max_pooling2d(inputs=new_conv_layer, pool_size=[pool_dim1, pool_dim2], strides=stride)
+        # print("New pool shape:", new_pool.shape)
 
         #Each layer's size is dependent on their poolsize, and so we save these for upsampling later
-        #upsample_sizes.append(layers.shape)
-        layers.append(new_conv_layer)
-        layers.append(new_pool)
-        upsample_sizes.append(new_pool.get_shape().as_list()[:-1])
+        #conv_input_shapes.append(layers.shape)
+        # layers.append(new_/conv_layer)
+        conv_layers.append(new_pool)
+        # conv_input_shapes.append(new_pool.get_shape())
+        last_layer = new_pool
+    # conv_input_shapes.pop()
 
     #upsample time!
-    for i, layer_size in enumerate(reversed(upsample_sizes[:-1])):
+    print("Generating hourglass upsample")
+    print("shapes: %s" % [x.shape.as_list() for x in conv_layers])
+    for i in range(len(conv_layers) - 1):
         #upsample by nearest neighbor
-        print(tf.reshape(layers[-1],upsample_sizes[-(i+1)]).shape)
-        new_upsample_layer = tf.image.resize_nearest_neighbor(tf.reshape(layers[-1],upsample_sizes[-(i+1)]),size=layer_size[1:3])
-        #if i == len(upsample_sizes)-2:
+        # print(last_layer.shape)
+        corresponding_layer = conv_layers[-(i+2)]
+        print(corresponding_layer)
+
+        upsampled_size = corresponding_layer.shape[1:3]
+        print("Targe shape: %s" % upsampled_size.as_list())
+
+        new_upsample_layer = tf.image.resize_nearest_neighbor(last_layer, size=upsampled_size)
+        #if i == len(conv_input_shapes)-2:
         #new_upsample_layer = tf.reshape(new_upsample_layer, [-1, 200, 200, 1])
         #add residual layer
-        residual_layer = tf.reshape(residual_module(layers[-(3+3*i)]),layer_size)
-        if i == len(upsample_sizes)-2:
-            new_upsample_layer = tf.reshape(new_upsample_layer, layer_size)
-        new_upsample_layer = tf.add(new_upsample_layer, residual_layer)
-        layers.append(new_upsample_layer)
-    
+        residual_layer = residual_module(corresponding_layer)
+
+        print("Upsampled layer shape: %s" % new_upsample_layer.shape)
+        print("Residual input shape: %s" % conv_layers[-i].shape)
+        print("Residual layer shape: %s" % residual_layer.shape)
+
+        # if i == len(conv_input_shapes)-2:
+        #     new_upsample_layer = tf.reshape(new_upsample_layer, layer_size)
+        last_layer = tf.add(new_upsample_layer, residual_layer)
+        # layers.append(new_upsample_layer)
+
 
     #finally, 200 1x1 convolutional layers and we're done
-    new_conv_layer = tf.layers.conv3d(
-        inputs=layers[-1],
+    new_conv_layer = tf.layers.conv2d(
+        inputs=last_layer,
         filters=200,
-        kernel_size=[1,1,3],
+        kernel_size=[1,1],
         activation=None)
-    new_conv_layer = tf.reshape(new_conv_layer, [-1, 200, 200, 200])
+    # new_conv_layer = tf.reshape(new_conv_layer, [-1, 200, 200, 200])
     print(new_conv_layer.shape)
-    layers.append(new_conv_layer)
+    last_layer = new_conv_layer
     #return our last layer, the output
-    return layers[-1]
+    return last_layer
 
 #given an input size and output size, find the right kernel size for CNN
 def get_kernel_size(layer_in, layer_out, padding="none"):
@@ -90,10 +108,10 @@ def hourglass_model_fn(features, labels, mode):
     pool_details = [(73, 73, 1, 1), (73, 73, 1, 1), (44, 44, 1, 1)]
 
     hourglass_model = get_hourglass(features, layer_details, pool_details, residual_model)
-    
+
     if mode == tf.estimator.ModeKeys.PREDICT:
         return hourglass_model
-    
+
     labels = tf.reshape(labels, [-1, 200, 200, 200])
     loss = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(logits=hourglass_model, labels=labels), name= 'cross_entropy_loss')
     if mode == tf.estimator.ModeKeys.TRAIN:
@@ -110,19 +128,14 @@ def train_model(path, train_data, train_labels):
     facelift = tf.estimator.Estimator(model_fn=hourglass_model_fn, model_dir=path)
     train_input_fn = tf.estimator.inputs.numpy_input_fn(x={"x": train_data},
         y=train_labels,
-        batch_size=10,
+        batch_size=2,
         num_epochs=None,
         shuffle=True)
     facelift.train(input_fn=train_input_fn, steps=20000)
     return facelift
 
 
-train_data = np.array([np.random.rand(200, 200, 3) for i in range(10)]).astype('float32')
-train_labels = np.array([np.random.rand(200, 200, 200) for i in range(10)]).astype('float32')
+train_data = np.array([np.random.rand(200, 200, 3) for i in range(2)]).astype('float32')
+train_labels = np.array([np.random.rand(200, 200, 200) for i in range(2)]).astype('float32')
 model_path = "hourglass_util/"
 train_model(model_path, train_data, train_labels)
-
-
-
-
-
