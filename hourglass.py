5a8440b8583634c6daea462b2dce2a168803edb3
wuisawesome <itswu.alex@gmail.com>
2018-04-21 18:47:46 -0700

    Cleaning up

--- hourglass.py
+++ hourglass.py
@@ -2,7 +2,7 @@ import tensorflow as tf
 import numpy as np
 import tensorflow.contrib.slim as slim
 import sklearn
-from manager import get_batch
+from DataManager.manager import get_batch
 
 '''
 RESBLOCK BOIZ
@@ -33,7 +33,7 @@ def get_hourglass(features, layer_details, pool_details, residual_module):
     conv_layers = [input_layer]
     # conv_input_shapes = [input_layer.get_shape()]
     last_layer = input_layer
-    print("Generating hourglass conv layers")
+
     for (kernel_dim1, kernel_dim2, kernel_dim3, nfilters, padding, activation), (pool_dim1, pool_dim2, pool_dim3, stride) in zip(layer_details, pool_details):
         new_conv_layer = tf.layers.conv2d(
             inputs=last_layer,
@@ -57,8 +57,6 @@ def get_hourglass(features, layer_details, pool_details, residual_module):
     # conv_input_shapes.pop()
 
     #upsample time!
-    print("Generating hourglass upsample")
-    print("shapes: %s" % [x.shape.as_list() for x in conv_layers])
     for i in range(len(conv_layers) - 1):
         #upsample by nearest neighbor
         # print(last_layer.shape)
@@ -66,17 +64,12 @@ def get_hourglass(features, layer_details, pool_details, residual_module):
         print(corresponding_layer)
 
         upsampled_size = corresponding_layer.shape[1:3]
-        print("Targe shape: %s" % upsampled_size.as_list())
 
         new_upsample_layer = tf.image.resize_nearest_neighbor(last_layer, size=upsampled_size)
         #if i == len(conv_input_shapes)-2:
         #new_upsample_layer = tf.reshape(new_upsample_layer, [-1, 200, 200, 1])
         #add residual layer
         residual_layer = residual_module(corresponding_layer)
-
-        print("Upsampled layer shape: %s" % new_upsample_layer.shape)
-        print("Residual input shape: %s" % conv_layers[-i].shape)
-        print("Residual layer shape: %s" % residual_layer.shape)
 
         # if i == len(conv_input_shapes)-2:
         #     new_upsample_layer = tf.reshape(new_upsample_layer, layer_size)
@@ -104,42 +97,3 @@ def get_kernel_size(layer_in, layer_out, padding="none"):
 def get_layer_size(layer_in, kernel, padding="none"):
     if padding == "none":
         return (layer_in[0]-kernel[0]+1,layer_in[1]-kernel[1]+1,layer_in[2]-kernel[2]+1)
-
-#given a path for saving progress for our model, training and label data, returns trained model.
-#this is where most of the training will take effect
-def train_model(path, train_data, train_labels, batch_size, iterations):
-    layers = [(200, 200, 3), (125, 125, 3), (50, 50, 3), (4, 4, 3)]
-    kernels = [(4, 4, 3), (4, 4, 3), (4, 4, 3)]
-    filters = [3 for i in range(len(layers)-1)]
-    padding = ["valid" for i in range(len(layers)-1)]
-    activation = [tf.nn.relu for i in range(len(layers)-1)]
-    
-    layer_details = [(kernels[i][0], kernels[i][1], kernels[i][2], filters[i], padding[i], activation[i]) for i in range(len(layers)-1)]
-    #residual_model = lambda x : x
-    residual_model = resBlock
-    pool_details = [(73, 73, 1, 1), (73, 73, 1, 1), (44, 44, 1, 1)]
-    
-    input = tf.placeholder(tf.float32, name="input", shape=(None, 200, 200, 3))
-    labels = tf.placeholder(tf.float32, name="labels", shape=(None, 200, 200, 200))
-
-    hourglass_model = get_hourglass(input, layer_details, pool_details, residual_model)
-    
-    loss = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(logits=hourglass_model, labels=labels), name= 'cross_entropy_loss')
-    train_step = tf.train.AdamOptimizer(1e-4).minimize(loss)
-    
-    with tf.Session() as sess:
-        sess.run(tf.global_variables_initializer())
-        for i in range(iterations):
-            print("iteration ", i)
-            batch = get_batch(batch_size)
-            train_step.run(feed_dict = {input: batch[0], labels: batch[1]})
-    
-    return hourglass_model
-
-
-train_data = np.array([np.random.rand(200, 200, 3) for i in range(2)]).astype('float32')
-train_labels = np.array([np.random.rand(200, 200, 200) for i in range(2)]).astype('float32')
-model_path = "hourglass_util/"
-
-
-train_model(model_path, train_data, train_labels, batch_size=100, iterations=1000)
---
+++ train.py
@@ -0,0 +1,42 @@
+from hourglass import *
+import tensorflow as tf
+import numpy as np
+
+#given a path for saving progress for our model, training and label data, returns trained model.
+#this is where most of the training will take effect
+def train_model(path, train_data, train_labels, batch_size, iterations):
+    layers = [(200, 200, 3), (125, 125, 3), (50, 50, 3), (4, 4, 3)]
+    kernels = [(4, 4, 3), (4, 4, 3), (4, 4, 3)]
+    filters = [3 for i in range(len(layers)-1)]
+    padding = ["valid" for i in range(len(layers)-1)]
+    activation = [tf.nn.relu for i in range(len(layers)-1)]
+
+    layer_details = [(kernels[i][0], kernels[i][1], kernels[i][2], filters[i], padding[i], activation[i]) for i in range(len(layers)-1)]
+    #residual_model = lambda x : x
+    residual_model = resBlock
+    pool_details = [(73, 73, 1, 1), (73, 73, 1, 1), (44, 44, 1, 1)]
+
+    input = tf.placeholder(tf.float32, name="input", shape=(None, 200, 200, 3))
+    labels = tf.placeholder(tf.float32, name="labels", shape=(None, 200, 200, 200))
+
+    hourglass_model = get_hourglass(input, layer_details, pool_details, residual_model)
+
+    loss = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(logits=hourglass_model, labels=labels), name= 'cross_entropy_loss')
+    train_step = tf.train.AdamOptimizer(1e-4).minimize(loss)
+
+    with tf.Session() as sess:
+        sess.run(tf.global_variables_initializer())
+        for i in range(iterations):
+            print("iteration ", i)
+            batch = get_batch(batch_size)
+            train_step.run(feed_dict = {input: batch[0], labels: batch[1]})
+
+    return hourglass_model
+
+
+train_data = np.array([np.random.rand(200, 200, 3) for i in range(2)]).astype('float32')
+train_labels = np.array([np.random.rand(200, 200, 200) for i in range(2)]).astype('float32')
+model_path = "hourglass_util/"
+
+
+train_model(model_path, train_data, train_labels, batch_size=100, iterations=1000)
